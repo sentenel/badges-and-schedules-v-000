14ce69bb81f7d96ee1f830bc1e769e670a1f259a
Seth Wax <sethlwax@gmail.com>
2018-03-20 08:44:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,10 @@ def assign_rooms(attendees)
   attendees.each_with_index{|name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
   room_assignments
 end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  room_assignments = assign_rooms(attendees)
+  badges.each{|badge| puts badge}
+  room_assignments.each{|room| puts room}
+end
